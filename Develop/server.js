0928e8118b4d7e374b10b263f9844b463c104a73
NoemiCouch <Noemicouch@gmail.com>
2023-06-02 20:46:46 -0500

    s

--- Develop/server.js
+++ Develop/server.js
@@ -4,9 +4,9 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 
-const port = Process.env.PORT || 3001;
-
 const app = express();
+
+const port = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
